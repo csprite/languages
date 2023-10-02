948edf824bdafeddcd30d46c8b97ffc1208e284a
pegvin <75035219+pegvin@users.noreply.github.com>
2023-10-02 14:22:57 +0530

    Switch To ini Format

--- ValidateLangFiles.py
+++ ValidateLangFiles.py
@@ -4,26 +4,47 @@
 import os
 import sys
 import glob
-import json
+import configparser
+
+ConfigStructure = {
+	"file_menu": [ "file", "new", "open" ],
+	"help_menu": [ "help", "about", "github" ],
+	"open_file_popup": [ "open_file" ],
+	"new_document_popup": [
+		"title", "width_input", "height_input",
+		"ok_button", "cancel_button"
+	],
+	"about_csprite_popup": [
+		"title", "contributors_header", "contributors_paragraph",
+		"contributors_link", "os_projects_header", "os_projects_text"
+	],
+	"unicode_range": [ "range" ]
+}
 
 Files = []
+ExitCode = 0
 
-for f in glob.glob("assets/languages/*.json"):
+for f in glob.glob("./*.ini"):
 	if os.path.isfile(f):
 		Files.append(f)
 
 for f in Files:
 	try:
-		fp = open(f, "rb")
-		j = json.load(fp)
-		if "UnicodeRange" in j:
-			ranges = j["UnicodeRange"]
-			for r in ranges:
-				if not r.startswith("0x"):
-					raise Exception(f"Invalid hexadecimal representation: \"{r}\"")
-				num = int(r, 16)
+		config=configparser.ConfigParser()
+		config.read(f)
+
+		for Section in ConfigStructure:
+			if not Section in config:
+				raise Exception(f"section '{Section}' not found")
+
+			for SubSection in ConfigStructure[Section]:
+				if not SubSection in config[Section]:
+					raise Exception(f"sub section '{SubSection}' of section '{Section}' not found")
 
 		print(f"{f} - ok")
 	except Exception as e:
 		print(f"{f} - {e}")
-		sys.exit(1)
+		ExitCode = 1
+
+sys.exit(ExitCode)
+
