fbc9c83fe9dc9d8b66ac826b5964ea323501311d
Aditya <75035219+pegvin@users.noreply.github.com>
2024-02-21 02:28:33 +0530

    Update Linter

---
+++ lint.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const ini = require("ini");
+const core = require("@actions/core");
+const process = require("process");
+const structure = require("./structure.js");
+
+const FilesToLint = fs.readdirSync("./").filter(function(filePath) {
+	return filePath.toLowerCase().endsWith(".ini");
+});
+
+var ExitCode = 0;
+
+FilesToLint.forEach(function(filePath) {
+	const Parsed = ini.parse(fs.readFileSync(filePath, 'utf-8'))
+	let warnings = 0;
+	let errors = 0;
+
+	// Ensure All Sections Defined in `structure`
+	// exist in the parsed ini file
+	Object.keys(structure).forEach(function(section) {
+		if (Parsed[section] == null) {
+			core.error(`Section '${section}' is required but not found`, { file: filePath });
+			errors++;
+		}
+	});
+
+	// Ensure All Sections in parsed ini file `Parsed` are known
+	Object.keys(Parsed).forEach(function(section) {
+		if (structure[section] == null) {
+			core.warning(`Unknown Section '${section}'`, { file: filePath });
+			warnings++;
+		} else {
+			// Ensure All Required Sub-Sections Defined in `structure[section]`
+			// exist in the parsed ini file
+			Object.keys(structure[section]).forEach(function(subSection) {
+				if (structure[section][subSection].required) {
+					if (Parsed[section][subSection] == null) {
+						core.error(`Sub-Section '${subSection}' in Section '${section}' is required but not found`, { file: filePath });
+						errors++;
+					}
+				}
+			});
+
+			// Ensure All Sub-Sections in parsed ini file `Parsed[section]` are known
+			Object.keys(Parsed[section]).forEach(function(subSection) {
+				if (structure[section][subSection] == null) {
+					core.warning(`Unknown Sub-Section '${subSection}' in Section '${section}'`, { file: filePath });
+					warnings++;
+				}
+			});
+		}
+	});
+
+	if (errors == 0) {
+		core.notice(`Ok (${warnings} ${warnings != 1 ? "Warnings" : "Warning"})`, { file: filePath });
+	} else {
+		ExitCode = 1;
+	}
+});
+
+process.exit(ExitCode);
---
+++ structure.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"file_menu": {
+		"file": { required: true },
+		"new":  { required: true },
+		"open": { required: true },
+	},
+	"help_menu": {
+		"help":   { required: true },
+		"about":  { required: true },
+		"github": { required: true }
+	},
+	"open_file_popup": {
+		"open_file": { required: true }
+	},
+	"new_document_popup": {
+		"title":         { required: true },
+		"width_input":   { required: true },
+		"height_input":  { required: true },
+		"ok_button":     { required: true },
+		"cancel_button": { required: true }
+	},
+	"about_csprite_popup": {
+		"title":                  { required: true },
+		"contributors_header":    { required: true },
+		"contributors_paragraph": { required: true },
+		"contributors_link":      { required: true },
+		"os_projects_header":     { required: true },
+		"os_projects_text":       { required: true }
+	},
+	"unicode_range": {
+		"range": { required: false }
+	}
+}
